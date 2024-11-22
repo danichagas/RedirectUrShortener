a125e7752185958ec4833f9dbcee488d585a8cf4
danichagas <chagasdaniel788@gmail.com>
2024-11-22 14:56:02 -0300

    feat: Transforming the .json into an object of type UrlData

--- src/main/java/com/danichagas/redirectUrlShortner/Main.java
+++ src/main/java/com/danichagas/redirectUrlShortner/Main.java
@@ -2,14 +2,18 @@ package com.danichagas.redirectUrlShortner;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 
+import java.io.InputStream;
 import java.util.Map;
 
 public class Main implements RequestHandler<Map<String, Object>, Map<String, String>> {
 
     public final S3Client s3Client = S3Client.builder().build();
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public Map<String, String> handleRequest(Map<String, Object>input, Context context) {
@@ -24,6 +28,22 @@ public class Main implements RequestHandler<Map<String, Object>, Map<String, Str
                 .bucket("url-shortener-storage-by-dani")
                 .key(shortUrlCode + ".json")
                 .build();
+
+        InputStream s3ObjectStream;
+
+        try {
+            s3ObjectStream = s3Client.getObject(getObjectRequest);
+        } catch (Exception exception) {
+            throw new RuntimeException("Error fetching URL data from S3: " + exception.getMessage());
+        }
+
+        UrlData urlData;
+
+        try {
+            urlData = objectMapper.readValue(s3ObjectStream, UrlData.class);
+        } catch (Exception exception) {
+            throw new RuntimeException("Error deserializing URL data: " + exception.getMessage());
+        }
 
         return null;
     }
