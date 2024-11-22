04cbe476e1a648f9f6a36af8b028c7c978297519
danichagas <chagasdaniel788@gmail.com>
2024-11-22 14:02:53 -0300

    feat: handleRequest method

--- src/main/java/com/danichagas/redirectUrlShortner/Main.java
+++ src/main/java/com/danichagas/redirectUrlShortner/Main.java
@@ -1,8 +1,14 @@
 package com.danichagas.redirectUrlShortner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import java.util.Map;
+
+public class Main implements RequestHandler<Map<String, Object>, Map<String, String>> {
+
+    @Override
+    public Map<String, String> handleRequest(Map<String, Object> stringObjectMap, Context context) {
+        return null;
     }
 }
